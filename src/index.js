316df5a8fc623eb69d768afaf4bf3fa902022fd5
ekoro061@uottawa.ca <ekoro061@uottawa.ca>
2024-03-15 22:07:47 -0400

    Added Navbar, page navigation, location page content

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React, {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import "./styles.css";
-
 import App from "./App";
+
+const rentLink = document.getElementById('rentLink');
+const faqLink = document.getElementById('faqLink');
+const locationsLink = document.getElementById('locationsLink');
+
+const rentDiv = document.getElementById('rentDiv');
+const faqDiv = document.getElementById('faqDiv');
+const locationsDiv = document.getElementById('locationsDiv');
+
 
 const root = createRoot(document.getElementById("root"));
 root.render(
@@ -10,3 +18,41 @@ root.render(
         <App />
     </StrictMode>
 );
+
+
+
+window.onload = () => {
+    faqDiv.classList.add('hide');
+    locationsDiv.classList.add('hide');
+    
+    if (rentDiv.classList.contains('hide')) {
+        rentDiv.classList.remove('hide');
+    }
+}
+
+rentLink.addEventListener('click', () => {
+    faqDiv.classList.add('hide');
+    locationsDiv.classList.add('hide');
+    
+    if (rentDiv.classList.contains('hide')) {
+        rentDiv.classList.remove('hide');
+    }
+});
+
+faqLink.addEventListener('click', () => {
+    rentDiv.classList.add('hide');
+    locationsDiv.classList.add('hide');
+
+    if (faqDiv.classList.contains('hide')) {
+        faqDiv.classList.remove('hide');
+    }
+});
+
+locationsLink.addEventListener('click', () => {
+    rentDiv.classList.add('hide');
+    faqDiv.classList.add('hide');
+
+    if (locationsDiv.classList.contains('hide')) {
+        locationsDiv.classList.remove('hide');
+    }
+});
