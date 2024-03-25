feef89c842c40daca93193115513da8dd09a8ff6
zechen <zhou2067522448@gmail.com>
2024-03-25 01:23:48 -0400

    Finish translation for 'Contact Us'

--- src/contactUsButton.js
+++ src/contactUsButton.js
@@ -9,7 +9,7 @@ function ContactUsButton() {
 
     <div class="container mt-5">
       <button class="btn btn-primary position-fixed bottom-0 end-0 m-5" type="button" data-bs-toggle="collapse" data-bs-target="#chatFormCollapse" aria-expanded="false" aria-controls="chatFormCollapse">
-        { t("contactUs_button_msg") }
+        {t("contactUs_button_msg")}
       </button>
 
       <div class="collapse mt-3 col-3 position-fixed bottom-0 end-0 m-0" id="chatFormCollapse">
@@ -17,23 +17,23 @@ function ContactUsButton() {
           <button type="button" class="btn-close position-absolute top-0 end-0 m-2" aria-label="Close" data-bs-toggle="collapse" data-bs-target="#chatFormCollapse" aria-expanded="false" aria-controls="chatFormCollapse"></button>
 
           <form>
-            <h5>Contact Us</h5>
+            <h5>{t("contactUs_button_msg")}</h5>
             <div class="mb-3">
-              Call 613-888-8888 OR send us your questions below and we will help you as soon as we can :)
+              {t("contactUs_msg")}
             </div>
             <div class="mb-3">
-              <label for="nameInput" class="form-label">Name</label>
+              <label for="nameInput" class="form-label">{t("name_label")}</label>
               <input type="text" class="form-control" id="nameInput" />
             </div>
             <div class="mb-3">
-              <label for="emailInput" class="form-label">Email</label>
+              <label for="emailInput" class="form-label">{t("email_label")}</label>
               <input type="text" class="form-control" id="emailInput" />
             </div>
             <div class="mb-3">
-              <label for="questionInput" class="form-label">Question</label>
+              <label for="questionInput" class="form-label">{t("question_label")}</label>
               <textarea class="form-control" id="questionInput" rows="3" />
             </div>
-            <button type="submit" class="btn btn-primary">Send</button>
+            <button type="submit" class="btn btn-primary">{t("send_button_msg")}</button>
           </form>
         </div>
       </div>
