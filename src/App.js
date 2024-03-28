c071374c61d36c976180d006d7e1cc01e77d8c7e
zechen <zhou2067522448@gmail.com>
2024-03-28 02:14:47 -0400

    Add progress bar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ContactUsButton from './contactUsButton'
 import LanguageSelector from './LanguageSelector';
+import MultiStepProgressBar from './components/MultiStepProgressBar';
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <ContactUsButton />
-                <LanguageSelector />
-            </div>
-        )
-    }
-}
+function App() {
+    // default state is 1
+    const [index, setIndex] = useState(1);
+  
+    return (
+      <div>
+        {/* className="h-100": A height of 100%  */}
+        <Container className="h-100">
+          <Row className='h-100'>
+            <Col className='align-self-center'>
+              <MultiStepProgressBar step={index}/>
+            </Col>
+          </Row>
+        </Container>
+
+        <LanguageSelector/>
+        <ContactUsButton/>
+      </div>
+    );
+  }
 
 export default App;
---
+++ src/components/MultiStepProgressBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+
+
+const MultiStepProgressBar = (props) => {
+
+    return (
+        <ProgressBar
+            percent={((props.step - 1) / 2 * 100)}
+            filledBackground="linear-gradient(to right, #fefb72, #f0bb31)"
+        >
+            {/* step 1 */}
+            <Step transition="scale">
+                {() => (
+                    <img className="step"
+                        width="66"
+                        src="./images/circle-1-active.png"
+                    />
+                )}
+            </Step>
+
+            {/* step 2 */}
+            <Step transition="scale">
+                {({ accomplished }) => (
+                    <img className="step"
+                        width="66"
+                        src={accomplished ? "./images/circle-2-active.png" : "./images/circle-2-inactive.png"}
+                    />
+                )}
+            </Step>
+
+            {/* step 3 */}
+            <Step transition="scale">
+                {({ accomplished }) => (
+                    <img className="step"
+                        width="66"
+                        src={accomplished ? "./images/circle-3-active.png" : "./images/circle-3-inactive.png"}
+                    />
+                )}
+            </Step>
+        </ProgressBar>
+    );
+}
+
+export default MultiStepProgressBar;
