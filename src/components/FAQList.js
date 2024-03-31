5930b47eb9f47dc7bbabfb9d03c6c053c5fbb731
zechen <zhou2067522448@gmail.com>
2024-03-31 03:46:28 -0400

    Add FAQ page

---
+++ src/components/FAQList.js
@@ -0,0 +1,24 @@
+const FAQList = [
+    {
+        question: "How do I rent a car?",
+        answer: "To rent a car with us, you can use the 'Rent' page on our website, book in person at one of our locations, or call/email our customer service team via the button at the bottom left of our website."
+    },
+    {
+        question: "What is the minimum age to rent a car?",
+        answer: "The minimum age to rent a car with us is 18. However, there is an extra insurance fee for drivers under the age of 25."
+    },
+    {
+        question: "What forms of payment do you accept?",
+        answer: "We accept Visa and MasterCard credit cards only."
+    },
+    {
+        question: "Can I add an additional driver to my rental?",
+        answer: "Yes, additional drivers can be added on vehicle pickup for a small fee. Additional drivers must bring their id and fill out a registration form on vehicle pickup."
+    },
+    {
+        question: "Can I return my rental car if the (Y)OUR CAR location is closed?",
+        answer: "Yes, we offer after-hours drop-off. Just park the car at any drop-off parking spot and drop the car key into the key drop-off box. Ask a representative for additional info when you pick up your rental car."
+    }
+]
+
+export default FAQList;
